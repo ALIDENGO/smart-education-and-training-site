056778a98151ace82def15c1b299be7f213c8d8d
ALIDENGO <ntfinance81@gmail.com>
2025-10-28 00:40:45 +0100

    add 404 not-found page

---
+++ src/app/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main className="container">
+      <h1>Page introuvable</h1>
+      <p>La page que vous cherchez n’existe pas.</p>
+      <p><a href="/">Revenir à l’accueil</a></p>
+    </main>
+  );
+}
